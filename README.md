d11424f1016ab86bf7decacc463141b0ca2c85f0
Carlos <nzlosh@yahoo.com>
2019-11-16 17:00:05 +0100

    Initial import of project.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # pack2md
-Readme generator for StackStorm packs.
+
+Readme generator for StackStorm packs.  This tool will read the pack meta data, config schema,
+action metadata and sensor metadata.  It will pass this information to a jinja2 template to
+populate and produce a README file in markdown format.
+
+## Installation
+
+1. Create a virtualenv.
+1. Install required modules.
+1. Edit `README.jinja` to fit the packs needs. (it should be located in the pack).
+1. Run `pack2dm.py <pack_path>`.
+
---
+++ pack2md.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+# pack2md.py creates pack README.md file from pack metadata.
+# Copyright (C) 2019  Carlos <nzlosh@yahoo.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import os
+import argparse
+
+import yaml
+from jinja2 import Environment, FileSystemLoader
+
+
+def fetch_template(template="README.jinja", template_dir="."):
+    print("Fetching template ...")
+    file_loader = FileSystemLoader(template_dir)
+    env = Environment(loader=file_loader)
+    return env.get_template(template)
+
+
+def fetch_yaml(dirname, display):
+    print("Fetching {} ...".format(display))
+    items = {}
+    if os.path.exists(dirname):
+        for e in os.listdir(dirname):
+            if e.find(".yaml") != -1:
+                with open("{}/{}".format(dirname, e), "r") as f:
+                    items[e] = yaml.load(f, Loader=yaml.FullLoader)
+    return items
+
+
+def render_document(template, data, filename):
+    if not os.path.exists(f"{filename}.bak"):
+        os.replace(f"{filename}", f"{filename}.bak")
+    with open(filename, "w") as f:
+        f.write(template.render(data))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Create README markdown from pack metadata.")
+    parser.add_argument("pack_path", type=str, help="Path to StackStorm pack.")
+    args = parser.parse_args()
+
+    if os.path.exists(args.pack_path):
+        render_document(
+            fetch_template(template_dir=args.pack_path),
+            {
+                "pack": fetch_yaml(args.pack_path, "pack meta"),
+                "actions": fetch_yaml(f"{args.pack_path}/actions", "action meta"),
+                "sensors": fetch_yaml(f"{args.pack_path}/sensors", "sensor meta")
+            },
+            f"{args.pack_path}/README.md"
+        )
+    else:
+        print("'{}' doesn't exist.".format(args.pack_path))
